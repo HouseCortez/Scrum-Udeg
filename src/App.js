cd701327a42d04c12f7eb3b7a31b6c5795fbe7e1
HouseCortez <ivan_house_20@hotmail.com>
2022-02-11 23:42:58 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './assets/images/Backlog.png';
+import './assets/css/App.css';
+
+//Importar componentes
+import MiComponente from './components/MiComponente';
+
+function HolaMundo(nombre, edad) {
+  var presentacion = (
+    <div>
+        <h2> Hola, soy {nombre}</h2>
+    </div>
+  );
+
+  return presentacion;
+}
+
+function App() {
+  var nombre = "Ivan Cortez";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>Hola, bienvenido a "Actividad integradora. Sprint, el corazón de SCRUM"</b>
+        </p>
+        {HolaMundo (nombre, 26)}
+        <section className="componentes" >
+
+            <MiComponente />
+           
+
+
+
+
+        </section>
+          
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MiComponente.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class MiComponente extends React.Component{
+    
+    render(){
+        return (
+            <React.Fragment>
+                <h1>Este es el product Backlog</h1>
+                <h3>Favor de leer atentamente</h3>
+            </React.Fragment>
+            
+            
+        );
+    }
+}
+
+export default MiComponente;
